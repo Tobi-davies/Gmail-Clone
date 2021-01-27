d61d3e7206216076ef27ccac3c75183db2356dcf
Adebayo <adebayotobiloba9@gmail.com>
2021-01-27 12:00:43 +0100

    added icons

--- src/components/sidebar/sidebar.component.jsx
+++ src/components/sidebar/sidebar.component.jsx
@@ -63,6 +63,97 @@ const SideBar = () => {
             <span className="title">Drafts</span>
             <span className="number">20</span>
           </li>
+
+          <li>
+            <span className="icon">
+              <img
+                src="https://www.gstatic.com/images/icons/material/system/1x/label_important_black_20dp.png"
+                alt="inbox"
+              />
+            </span>
+            <span className="title">Important</span>
+          </li>
+
+          <li>
+            <span className="icon">
+              <img
+                src="https://www.gstatic.com/images/icons/material/system/1x/label_important_black_20dp.png"
+                alt="inbox"
+              />
+            </span>
+            <span className="title">Chats</span>
+          </li>
+
+          <li>
+            <span className="icon">
+              <img
+                src="https://www.gstatic.com/images/icons/material/system/1x/schedule_send_black_20dp.png"
+                alt="inbox"
+              />
+            </span>
+            <span className="title">Scheduled</span>
+          </li>
+
+          <li>
+            <span className="icon">
+              <img
+                src="https://www.gstatic.com/images/icons/material/system/1x/mail_black_20dp.png"
+                alt="inbox"
+              />
+            </span>
+            <span className="title">All Mail</span>
+          </li>
+
+          <li>
+            <span className="icon">
+              <img
+                src="https://www.gstatic.com/images/icons/material/system/1x/report_black_20dp.png"
+                alt="inbox"
+              />
+            </span>
+            <span className="title">Spam</span>
+            <span className="number">2</span>
+          </li>
+
+          <li>
+            <span className="icon">
+              <img
+                src="https://www.gstatic.com/images/icons/material/system/1x/delete_black_20dp.png"
+                alt="inbox"
+              />
+            </span>
+            <span className="title">Trash</span>
+          </li>
+
+          <li>
+            <span className="icon">
+              <img
+                src="https://www.gstatic.com/images/icons/material/system/1x/inbox_gm_googlered600_20dp.png"
+                alt="inbox"
+              />
+            </span>
+            <span className="title">Create new label</span>
+          </li>
+
+          <li>
+            <span className="icon manage-icon">
+              <img
+                src="https://www.gstatic.com/images/icons/material/system/1x/settings_black_20dp.png"
+                alt="inbox"
+              />
+            </span>
+            <span className="title">Manage labels</span>
+          </li>
+
+          <li>
+            <span className="icon">
+              <img
+                src="https://www.gstatic.com/images/icons/material/system/1x/add_black_20dp.png"
+                alt="inbox"
+              />
+            </span>
+            <span className="title">Create new label</span>
+          </li>
         </ul>
       </div>
     </div>
