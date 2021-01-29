63043cdb98b221c4bc65fd2cb41b888a348be452
Adebayo <adebayotobiloba9@gmail.com>
2021-01-29 18:11:29 +0100

    paginated

--- src/components/category-buttons/category-button.component.jsx
+++ src/components/category-buttons/category-button.component.jsx
@@ -1,13 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import "./category-button.styles.css";
 
-const CategoryBtn = () => {
+const CategoryBtn = ({ paginate }) => {
+  const [red, setRed] = useState(true);
+  const [green, setGreen] = useState(false);
+  const [blue, setBlue] = useState(false);
+  const handleChange1 = () => {
+    setRed(true);
+    setGreen(false);
+    setBlue(false);
+  };
+
+  const handleChange2 = () => {
+    setRed(false);
+    setGreen(false);
+    setBlue(true);
+  };
+
+  const handleChange3 = () => {
+    setRed(false);
+    setGreen(true);
+    setBlue(false);
+  };
+
   return (
     <div className="category-btn">
-      <button>TEST1</button>
-      <button className="button2">TEST2</button>
-      <button className="button3">TEST3</button>
+      <button
+        className={red ? "button1" : "button1 blank"}
+        onClick={handleChange1}
+        onClick={() => paginate(1)}
+      >
+        TEST1
+      </button>
+      <button
+        className={blue ? "button2 blue" : "button2 "}
+        onClick={handleChange2}
+      >
+        TEST2
+      </button>
+      <button
+        className={green ? "button3 green" : "button3 "}
+        onClick={handleChange3}
+        onClick={() => paginate(3)}
+      >
+        TEST3
+      </button>
     </div>
   );
 };
 
 export default CategoryBtn;
+
+// const Pagination = ({ pageLimit, newLength, paginate, currentPage }) => {
+//   const pageNumbers = [];
+//   const averagePost = Math.ceil(newLength / pageLimit);
+//   for (let i = 1; i < averagePost + 1; i++) {
+//     pageNumbers.push(i);
+//   }
+
+//   console.log(averagePost);
+
+//   return (
+//     <nav className="paginated">
+//       {currentPage > 1 ? (
+//         <li className="prev" onClick={() => paginate(currentPage - 1)}>
+//           PREV
+//         </li>
+//       ) : (
+//         ""
+//       )}
+//       {pageNumbers.map((num) => (
+//         <li key={num} className="page-link" onClick={() => paginate(num)}>
+//           {num}
+//         </li>
+//       ))}
+
+//       {currentPage < averagePost ? (
+//         <li className="prev" onClick={() => paginate(currentPage + 1)}>
+//           NEXT
+//         </li>
+//       ) : (
+//         ""
+//       )}
+//     </nav>
+//   );
+// };
--- src/components/inbox-content/inbox-content.component.jsx
+++ src/components/inbox-content/inbox-content.component.jsx
@@ -1,11 +1,33 @@
 import React, { useState } from "react";
 import "./inbox-content.styles.css";
 import DATA from "../../data";
+import CategoryBtn from "../category-buttons/category-button.component";
 
 const InboxContent = () => {
   const [data] = useState(DATA);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageLimit] = useState(10);
 
-  return <div className="inbox-content">{data.map((data) => data.name)}</div>;
+  const indexOfLastPost = currentPage * pageLimit;
+  const indexOfFirstPost = indexOfLastPost - pageLimit;
+
+  let currentMessages = null;
+
+  currentMessages = data.slice(indexOfFirstPost, indexOfLastPost);
+  console.log(currentMessages);
+  console.log(data);
+  console.log(currentPage);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  return (
+    <div className="inbox-content">
+      <CategoryBtn paginate={paginate} />
+      {currentMessages.map((data) => (
+        <div>{data.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default InboxContent;
