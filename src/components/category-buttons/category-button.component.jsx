a8c431644e48587ecdd49f43c9a4954b471b4354
Adebayo <adebayotobiloba9@gmail.com>
2021-01-29 22:15:46 +0100

    paginated

--- src/components/category-buttons/category-button.component.jsx
+++ src/components/category-buttons/category-button.component.jsx
@@ -32,19 +32,62 @@ const CategoryBtn = ({ paginate }) => {
         className={red ? "button button1" : "button button1 blank"}
         onClick={handleChange1}
       >
-        Primary
+        <div className="btn-img">
+          {red ? (
+            <img src="https://www.gstatic.com/images/icons/material/system/1x/inbox_gm_googlered600_20dp.png" />
+          ) : (
+            <img
+              src="https://www.gstatic.com/images/icons/material/system/1x/inbox_black_20dp.png"
+              alt="img"
+              className="dull"
+            />
+          )}
+        </div>
+        <p>Primary</p>
+        <div className="border-red"></div>
       </button>
       <button
         className={blue ? "button button2 blue" : "button button2"}
         onClick={handleChange2}
       >
-        TEST2
+        <div className="btn-img">
+          {blue ? (
+            <img src="https://www.gstatic.com/images/icons/material/system/1x/people_gm_blue600_20dp.png" />
+          ) : (
+            <img
+              src="https://www.gstatic.com/images/icons/material/system/1x/people_black_20dp.png"
+              alt="img"
+              className="dull"
+            />
+          )}
+        </div>
+
+        <div className="btn-info">
+          <div className="top">
+            <p className="title">Social</p>
+            <p className="new new-social">2 new</p>
+          </div>
+          <div className="bottom">Twitter, Facebook</div>
+        </div>
+        <div className="border-blue"></div>
       </button>
       <button
         className={green ? "button button3 green" : "button3 button"}
         onClick={handleChange3}
       >
-        TEST3
+        <div className="btn-img">
+          {green ? (
+            <img src="https://www.gstatic.com/images/icons/material/system/1x/local_offer_gm_green700_20dp.png" />
+          ) : (
+            <img
+              src="https://www.gstatic.com/images/icons/material/system/1x/local_offer_black_20dp.png"
+              alt="img"
+              className="dull"
+            />
+          )}
+        </div>
+        <p className="title">Promotions</p>
+        <div className="border-green"></div>
       </button>
     </div>
   );
