f3b6ed3d8c96643b92785c7c7071e45a66f79656
Adebayo <adebayotobiloba9@gmail.com>
2021-03-20 17:19:51 +0100

    updated database

--- src/data.js
+++ src/data.js
@@ -5,12 +5,14 @@ const NAME_DATA = [
     title: "How to Access Job Opportunities in Tech + Opportunities",
     body:
       "Open this email. View this email in your broswer. Hello Tobiloba, Happy New Month",
+    date: "Feb 14",
   },
   {
     id: 2,
     name: "Codewars",
     title: "Your Weekly Codewars Kata",
     body: "Check out these featured codewars kata to help you get warmed up",
+    date: "Feb 11",
   },
   {
     id: 3,
@@ -18,6 +20,7 @@ const NAME_DATA = [
     title: "Tobiloba, here’s a chance to save when you need to move",
     body:
       "It’s still safer to stay at home. But we wanted you to know that we’re here for you with great prices if and when you do need to move.",
+    date: "Feb 6",
   },
   {
     id: 4,
@@ -25,6 +28,7 @@ const NAME_DATA = [
     title: "Tobenz just joined your workspace!",
     body:
       "Head to Slack to say hi to Tobenz Hop over to Slack to connect with Tobenz, who just arrived!",
+    date: "Feb 5",
   },
   {
     id: 5,
@@ -32,6 +36,7 @@ const NAME_DATA = [
     title: "falana mentioned you in Hashnode",
     body:
       "Want push notifications instead? Download Discord on your phone to keep chatting while AFK, or turn off these notifications now.",
+    date: "Jan 31",
   },
   {
     id: 6,
@@ -39,6 +44,7 @@ const NAME_DATA = [
     title: "[Firebase] Client access to your Realtime Database has expired",
     body:
       "[Firebase] Client access to your Realtime Database expired 58 day(s) ago",
+    date: "Jan 28",
   },
   {
     id: 7,
@@ -46,6 +52,7 @@ const NAME_DATA = [
     title:
       "Learn Data Visualization Skills [Free 17-hour Course on React and D3.js]",
     body: "Here are this week's five links that are worth your time:",
+    date: "Jan 27",
   },
   {
     id: 8,
@@ -53,12 +60,14 @@ const NAME_DATA = [
     title: "You missed messages in Junior Dev Forum",
     body:
       "Want push notifications instead? Download Discord on your phone to keep chatting while AFK, or turn off these notifications now.",
+    date: "Jan 27",
   },
   {
     id: 9,
     name: "Pluralsight",
     title: "Which skills will you need next?",
     body: "Develop new skills you can start using today",
+    date: "Jan 26",
   },
 
   {
@@ -66,6 +75,7 @@ const NAME_DATA = [
     name: "Heroku",
     title: "Maintain your app’s health with logging add-ons.",
     body: "Get greater visibilty into your apps's perfromance",
+    date: "Jan 25",
   },
   {
     id: 11,
@@ -79,6 +89,7 @@ const NAME_DATA = [
     name: "Coursera",
     title: "Explore new courses",
     body: "A special offer for you and a friend",
+    date: "Jan 11",
   },
   {
     id: 13,
@@ -86,6 +97,7 @@ const NAME_DATA = [
     title:
       "How to Create Lightning Fast Sites & Apps with Netlify + the Jamstack",
     body: "Learn the benefits of Netlify",
+    date: "Jan 6",
   },
   {
     id: 14,
@@ -93,6 +105,7 @@ const NAME_DATA = [
     title: "Renew ONLINE Today 7th February 2021 and Get 100% Data Bonus",
     body:
       "Dear Valued Customer, Enjoy your Sunday with More Data! Renew your account ONLINE TODAY 7th February 2021 and enjoy 100% Double data bonus at No EXTRA COST.",
+    date: "Jan 2",
   },
   {
     id: 15,
@@ -100,6 +113,7 @@ const NAME_DATA = [
     title: "Be your own boss, Tobias",
     body:
       "You could earn with us by signing up to drive. Sign up to drive with us in 4 simple steps",
+    date: "12/27/20",
   },
   {
     id: 16,
@@ -107,12 +121,14 @@ const NAME_DATA = [
     title: "New submission from Porfolio Email",
     body:
       "NEW FORM SUBMISSION Hey there, Someone just submitted your form on tobilobadev.netlify.app/src/contact.html. Here's what they had to say:",
+    date: "12/23/20",
   },
   {
     id: 17,
     name: "Codewars",
     title: "Your weekly Codewars Kata Featuring LA Hacks!",
     body: "Check out these featured Codewars kata to",
+    date: "Feb 22",
   },
   {
     id: 18,
@@ -121,102 +137,128 @@ const NAME_DATA = [
       "Take your conversations with you. Be notified when you get a reply.",
     body:
       "Try linkedIn's free app, Build relationships that advance your career",
+    date: "Feb 14",
   },
   {
     id: 19,
     name: "LinkedIn",
-    title: "Tobias, you're getting noticed",
+    title: "Hey, you're getting noticed",
     body: "See who's looking at your profile",
+    date: "Jan 24",
   },
   {
     id: 20,
     name: "LinkedIn",
-    title: "Tobias, thanks for being a valued member",
+    title: "Hey, thanks for being a valued member",
     body: "Enjoy your free trial",
+    date: "Jan 12",
   },
   {
     id: 21,
     name: "Facebook",
     title: "Chukwuma Ciroma shared a link",
     body: "See the post that he shared and engage",
+    date: "Jan 10",
   },
   {
     id: 22,
     name: "Facebook",
     title: "Rexxie Pexxie tagged you in a photo",
     body: "View on facebook",
+    date: "Jan 4",
   },
   {
     id: 23,
     name: "Facebook",
     title: "It's Marco Marco's birthday today",
     body: "Help Marco celebrate his birthday",
+    date: "Jan 1",
   },
   {
     id: 24,
-    name: "Floral Blouse",
-    title: "https://i.ibb.co/4W2DGKm/floral-blouse.png",
-    body: 20,
+    name: "Facebook",
+    title: "It's Taco's birthday today",
+    body: "Help Taco celebrate his birthday",
+    date: "12/23/20",
   },
   {
     id: 25,
-    name: "Floral Dress",
-    title: "https://i.ibb.co/KV18Ysr/floral-skirt.png",
-    body: 80,
+    name: "LinkedIn",
+    title: "Hey, you're getting noticed",
+    body: "See who's looking at your profile",
+    date: "11/21/20",
   },
   {
     id: 26,
-    name: "Red Dots Dress",
-    title: "https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png",
-    body: 80,
+    name: "Uche from bolt",
+    title: "Stay safe during the holidays",
+    body:
+      "As we celebrate the holidays and say goodbye to 2020, it is important for all of us to remain safety-conscious. ",
+    date: "11/03/20",
   },
+
   {
     id: 27,
-    name: "Striped Sweater",
-    title: "https://i.ibb.co/KmSkMbH/striped-sweater.png",
-    body: 45,
+    name: "SPECTRANET TEAM",
+    title: "100% DATA BONUS OFFER - ONLINE Renewal",
+    body: "ENJOY MORE! Renew your account ONLINE TODAY and get 100% bonus",
+    date: "10/22/20",
   },
   {
     id: 28,
-    name: "Yellow Track Suit",
-    title: "https://i.ibb.co/v1cvwNf/yellow-track-suit.png",
-    body: 135,
+    name: "Slack",
+    title: "Slack account sign in from a new device",
+    body:
+      "Slack account sign in from a new browser. If this was you, you are all set",
+    date: "10/09/20",
   },
   {
     id: 29,
-    name: "White Blouse",
-    title: "https://i.ibb.co/qBcrsJg/white-vest.png",
-    body: 20,
+    name: "Slack",
+    title: "Slack account sign in from a new device",
+    body:
+      "Slack account sign in from a new browser. If this was you, you are all set",
+    date: "10/08/20",
   },
   {
     id: 30,
-    name: "Camo Down Vest",
-    title: "https://i.ibb.co/xJS0T3Y/camo-vest.png",
-    body: 325,
+    name: "Slack",
+    title: "Slack account sign in from a new device",
+    body:
+      "Slack account sign in from a new browser. If this was you, you are all set",
+    date: "10/06/20",
   },
   {
     id: 31,
-    name: "Floral T-shirt",
-    title: "https://i.ibb.co/qMQ75QZ/floral-shirt.png",
-    body: 20,
+    name: "Twitter",
+    title: "Twitter account sign in from a new device",
+    body:
+      "Twitter account sign in from a new browser. If this was you, you are all set",
+    date: "10/06/20",
   },
   {
     id: 32,
-    name: "Black & White Longsleeve",
-    title: "https://i.ibb.co/55z32tw/long-sleeve.png",
-    body: 25,
+    name: "Twitter",
+    title: "Twitter account sign in from a new device",
+    body:
+      "Twitter account sign in from a new browser. If this was you, you are all set",
+    date: "10/04/20",
   },
   {
     id: 33,
-    name: "Pink T-shirt",
-    title: "https://i.ibb.co/RvwnBL8/pink-shirt.png",
-    body: 25,
+    name: "Twitter",
+    title: "Twitter account sign in from a new device",
+    body:
+      "Twitter account sign in from a new browser. If this was you, you are all set",
+    date: "10/01/20",
   },
   {
     id: 34,
-    name: "Jean Long Sleeve",
-    title: "https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png",
-    body: 40,
+    name: "Twitter",
+    title: "Twitter account sign in from a new device",
+    body:
+      "Twitter account sign in from a new browser. If this was you, you are all set",
+    date: "9/29/20",
   },
   {
     id: 35,
@@ -224,6 +266,7 @@ const NAME_DATA = [
     title: "New Career Paths. New career possibilities",
     body:
       "Fast-track your learning with new paths for Data Analysis and Data Science",
+    date: "Feb 12",
   },
   {
     id: 36,
@@ -231,36 +274,43 @@ const NAME_DATA = [
     title: "Make your designs move",
     body:
       "Surprise and delight with animation Weekly Newsletter Bring your designs and",
+    date: "Jan 15",
   },
   {
     id: 37,
     name: "Adobe Creative cloud",
     title: "20+ creative apps for the price of a single one",
     body: "Get 20+ Adobe apps with Creative Cloud. Get 2 months+",
+    date: "Jan 14",
   },
   {
     id: 38,
     name: "Lily at FutureLearn",
     title: "Courses to impress you 😍 (and employers!)",
     body: "Trending courses that we think you might like",
+    date: "Jan 10",
   },
+
   {
     id: 39,
     name: "Eva Product School",
     title: "Spotted",
     body: "Fresh products jobs from all over the world",
+    date: "Jan 3",
   },
   {
     id: 40,
     name: "Grammarly Insights",
     title: "No writing activity last week?",
     body: "Lets get back to it! Make sure you're logged in",
+    date: "12/22/20",
   },
   {
     id: 41,
     name: "Codepen",
     title: "The CodePen Spark: Popsicles, Panels, and Pugs",
     body: "The issue of the CodePen Spark is brought",
+    date: "12/10/20",
   },
   {
     id: 42,
@@ -268,6 +318,7 @@ const NAME_DATA = [
     title: "Stay safe during the holidays",
     body:
       "As we celebrate the holidays and say goodbye to 2020, it is important for all of us to remain safety-conscious. ",
+    date: "11/19/20",
   },
 
   {
@@ -275,36 +326,42 @@ const NAME_DATA = [
     name: "SPECTRANET TEAM",
     title: "100% DATA BONUS OFFER - ONLINE Renewal",
     body: "ENJOY MORE! Renew your account ONLINE TODAY and get 100% bonus",
+    date: "11/08/20",
   },
   {
     id: 44,
     name: "Heroku",
     title: "Deploy with confidence: create a pipeline",
     body: "Learn how Heroku Pipelines can help you deploy efficient apps",
+    date: "11/06/20",
   },
   {
     id: 45,
     name: "PluralSight",
     title: "Get 33% off for a limited time only",
     body: "It's one of our best deals of the year",
+    date: "10/05/20",
   },
   {
     id: 46,
     name: "Lily at FutureLearn",
     title: "DON’T MISS THIS: 30% off FutureLearn Unlimited!",
     body: "30% off as may short courses and digital ",
+    date: "10/04/20",
   },
   {
     id: 47,
     name: "Codepen",
     title: "The CodePen Spark: Paper, Patterns, and Pop",
     body: "View thios issue on CodePen Paper, Patterns and Pop",
+    date: "10/02/20",
   },
   {
     id: 48,
     name: "Lily at FutureLearn",
     title: "DON’T MISS THIS: 30% off FutureLearn Unlimited!",
     body: "30% off as may short courses and digital ",
+    date: "9/30/20",
   },
   {
     id: 49,
@@ -312,6 +369,7 @@ const NAME_DATA = [
     title: "Stay safe during the holidays",
     body:
       "As we celebrate the holidays and say goodbye to 2020, it is important for all of us to remain safety-conscious. ",
+    date: "9/29/20",
   },
 
   {
@@ -319,6 +377,7 @@ const NAME_DATA = [
     name: "SPECTRANET TEAM",
     title: "100% DATA BONUS OFFER - ONLINE Renewal",
     body: "ENJOY MORE! Renew your account ONLINE TODAY and get 100% bonus",
+    date: "9/28/20",
   },
 ];
 
